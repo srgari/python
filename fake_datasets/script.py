5ec803e56695c0fad423f8ec777a57e47dbc0c7f
sr_gari <sergio.marinho.silva@gmail.com>
2020-12-30 00:25:26 -0500

    criado id_cliente com hash de nome sobrenome cpf

--- fake_datasets/script.py
+++ fake_datasets/script.py
@@ -13,6 +13,8 @@ import numpy as np
 import re 
 import requests
 from bs4 import BeautifulSoup
+
+np.random.seed(42)
 # %%
 #### PARTE 1: NOMES E SEXO DOS CLIENTES: #####
 ### UTILIZANDO O DATASET https://brasil.io/dataset/genero-nomes/files/ 
@@ -52,7 +54,7 @@ lista_cpfs = [str(x) for x in np.random.randint(300000000,399999999,len(dataset)
 
 for x in range(len(lista_cpfs)):
     lala = sum([(int(y)*z) for y,z in zip(lista_cpfs[x], range(10,1,-1))])*10%11
-    
+    lala = lala if int(lala) < 10 else '0'
     lista_cpfs[x] = lista_cpfs[x] + str(lala)
     lele = str(sum([(int(y)*z) for y,z in zip(lista_cpfs[x], range(11,1,-1))])*10%11)
     lele = lele if int(lele) < 10 else '0'
@@ -62,6 +64,13 @@ lista_cpfs
 
 # %%
 dataset.insert(3, 'cpf', lista_cpfs)
-# %%
+#%%
+dataset.insert(0, 'id_cliente', (dataset['nome'] + dataset['sobrenome'] + dataset['cpf']).apply(hash))
+
+#%%
 dataset
+
+
+# %%
+dataset.to_csv('csv/dados_clientes.csv', index = False)
 # %%
