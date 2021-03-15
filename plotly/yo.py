7e48413c278d8b753c2a9e338d0a70c658cce986
sergio_windows <sergio.marinho.silva@gmail.com>
2021-03-14 23:29:58 -0300

    yo plotly yo

---
+++ plotly/yo.py
@@ -0,0 +1,44 @@
+#%%
+import requests
+import pandas as pd 
+from plotly import graph_objs as go
+import io 
+# %%
+#%%
+
+req = requests.get('https://data.brasil.io/dataset/covid19/caso.csv.gz')
+
+with open('lala.csv.gz', 'wb') as x:
+    x.write(req.content)
+
+#%%
+df = pd.read_csv('lala.csv.gz')#[['date','state','confirmed','deaths']]
+df2 = df.groupby(['date','state'])['confirmed','deaths'].sum()
+df2
+# %%
+df_sp = df2.query("state == 'SP'")
+df_sp.reset_index(inplace = True)
+df_sp
+# %%
+fig = go.Figure(data = go.Bar(y = df_sp.deaths.values,
+                    text =  [f'dia: {x}; mortes: {y}' for x,y in zip(df_sp.date.values,df_sp.deaths.values)],
+                    hoverinfo = 'text'
+                   ))
+                   
+
+fig.update_layout(
+    title = 'Mortes em sp',
+    xaxis = dict(
+        tickmode = 'array',
+        tickvals = list(range(1,len(df_sp),10)),
+        ticktext = df_sp.date.values[1:len(df_sp):10]  ))
+
+
+    
+#%%
+fig.write_html('covid_sp_deaths.html', auto_open=True)
+# %%
+df_sp
+
+
+# %%
